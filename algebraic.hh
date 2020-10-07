58e71f44f09160b69e587d43f6ce3102e451af86
Huck <huckkim2001@gmail.com>
2020-10-06 18:51:47 -0700

    updated concept definitions

--- algebraic.hh
+++ algebraic.hh
@@ -4,82 +4,15 @@
 #include <concepts>
 
 namespace schoof{
-
-/**
- * Requirements of FieldElement
- * 
- * A FieldElement requires elements be closed under addition and multiplication
- * as well as division defined for all values except the zero();
- */
-template<typename T>
-concept FieldElement = requires(T a, T b){
-    {-a}->std::same_as<T>;
-    {a+b}->std::same_as<T>;
-    {a*b}->std::same_as<T>;
-    {a/b}->std::same_as<T>;
-    {a==b}->std::same_as<bool>;
-    a+=b;
-    a*=b;
-};
-
-/**
- * Requirements of Field
- * 
- * A Ring requires elements from FieldElement and an additive/multiplicative identity
- */
-template<typename R>
-concept Field = requires(R a){
-    typename R::element;
-    {a.zero()}->std::same_as<typename R::element>;
-    {a.one()}->std::same_as<typename R::element>;
-} && FieldElement<typename R::element>;
-
-/**
- * Requirements of RingElement
- * 
- * A RingElement requires elements be closed under addition and multiplication
- */
-template<typename T>
-concept RingElement = requires(T a, T b){
-    {-a}->std::same_as<T>;
-    {a+b}->std::same_as<T>;
-    {a*b}->std::same_as<T>;
-    {a==b}->std::same_as<bool>;
-    a+=b;
-    a*=b;
-};
-
-/**
- * Requirements of Ring
- * 
- * A Ring requires elements from RingElement and an additive/multiplicative identity
- */
-template<typename R>
-concept Ring = requires(R a){
-    typename R::element;
-    {a.zero()}->std::same_as<typename R::element>;
-    {a.one()}->std::same_as<typename R::element>;
-} && RingElement<typename R::element>;
-
-/**
- * Requirements of a Rng
- * 
- * A Rng requires elements from RingElement and an additive identity
- */
-template<typename R>
-concept Rng = requires(R a, R b){
-    typename R::element;
-    {a.zero()}->std::same_as<typename R::element>;
-} && RingElement<typename R::element>;
-
 /**
  * Requirements of a GroupElement
  * 
  * A GroupElement requires the elements to be closed under addition
  */
-template<typename T>
-concept GroupElement = requires(T a, T b){
-    {a+b}->std::convertible_to<T>;
+template<typename GE>
+concept GroupElement = requires(GE a, GE b){
+    {-a}->std::same_as<GE>;
+    {a+b}->std::same_as<GE>;
     {a==b}->std::same_as<bool>;
 };
 
@@ -89,11 +22,63 @@ concept GroupElement = requires(T a, T b){
  * A Group requires elements from GroupElements and an additive identity
  */
 template<typename G>
-concept Group = requires(G a){
+concept Group = requires(G g){
     typename G::element;
-    {a.zero()}->std::same_as<typename G::element>;
+    {g.zero()}->std::same_as<typename G::element>;
 } && GroupElement<typename G::element>;
 
+/**
+ * Requirements of RingElement
+ * 
+ * A RingElement requires elements be closed under addition and multiplication
+ */
+template<typename RE>
+concept RingElement = requires(RE a, RE b){
+    {-a}->std::same_as<RE>;
+    {a+b}->std::same_as<RE>;
+    {a*b}->std::same_as<RE>;
+    {a==b}->std::same_as<bool>;
+};
+
+/**
+ * Requirements of Ring
+ * 
+ * A Ring requires elements from RingElement and an additive/multiplicative identity
+ */
+template<typename R>
+concept Ring = requires(R r){
+    typename R::element;
+    {r.zero()}->std::same_as<typename R::element>;
+    {r.one()}->std::same_as<typename R::element>;
+} && RingElement<typename R::element>;
+
+/**
+ * Requirements of FieldElement
+ * 
+ * A FieldElement requires elements be closed under addition and multiplication
+ * as well as division defined for all values except the zero();
+ */
+template<typename FE>
+concept FieldElement = requires(FE a, FE b){
+    {-a}->std::same_as<FE>;
+    {a.inv()}->std::same_as<FE>
+    {a+b}->std::same_as<FE>;
+    {a*b}->std::same_as<FE>;
+    {a/b}->std::same_as<FE>;
+    {a==b}->std::same_as<bool>;
+};
+
+/**
+ * Requirements of Field
+ * 
+ * A Field requires elements from FieldElement and an additive/multiplicative identity
+ */
+template<typename F>
+concept Field = requires(F f){
+    typename F::element;
+    {f.zero()}->std::same_as<typename F::element>;
+    {f.one()}->std::same_as<typename F::element>;
+} && FieldElement<typename R::element>;
 }
 
 #endif
